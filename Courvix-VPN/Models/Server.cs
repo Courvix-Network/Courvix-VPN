7cdc552ae55f614cc15124a0c62634d73348efbd
ToshiroZ <48633791+ToshiroZ@users.noreply.github.com>
2022-01-20 17:21:45 -0500

    Better OpenVPN Finding
    Improved the way openvpn is found.
    Also geared the code for localization by putting moving the strings to resources

--- Courvix-VPN/Models/Server.cs
+++ Courvix-VPN/Models/Server.cs
@@ -9,8 +9,26 @@ namespace Courvix_VPN.Models
 {
     public struct Server
     {
-        [JsonProperty("servername")] public string ServerName { get; set; }
-        [JsonProperty("url")] public string ConfigLink { get; set; }
+        /*
+        
+    "flagurl": "https://www.countryflags.com/wp-content/uploads/united-states-of-america-flag-png-large",
+    "flagurl_small": "https://www.countryflags.io/us/flat/64.png",
+    "provider": "NFOServers.com",
+    "protection": "NFOServers.com",
+    "countrycode": "US",
+    "enabled": true,
+    "down": false,
+    "url": "https://courvix.com/vpn/configs/NewYorkNFO.ovpn",
+    "servername": "New York 1"*/
+        [JsonProperty("flagurl")] public string FlagUrl { get; set; }
+        [JsonProperty("flagurl_small")] public string FlagUrlSmall { get; set; }
+        [JsonProperty("provider")] public string Provider { get; set; }
+        [JsonProperty("protection")] public string Protection { get; set; }
+        [JsonProperty("countrycode")] public string CountryCode { get; set; }
         [JsonProperty("enabled")] public bool Enabled { get; set; }
+        [JsonProperty("down")] public bool Down { get; set; }
+        [JsonProperty("url")] public string ConfigLink { get; set; }
+        [JsonProperty("servername")] public string ServerName { get; set; }
+       
     }
 }
